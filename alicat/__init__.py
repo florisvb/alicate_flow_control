04883034a52f58658b3be57843a14496d211e4ed
brainhacker <brainhacker@lobula.(none)>
2015-09-13 14:06:52 -0700

    added sleep and flush to set function, no crashing now

--- alicat/__init__.py
+++ alicat/__init__.py
@@ -59,6 +59,8 @@ class FlowMeter(object):
         command = "*@={addr}\r\n".format(addr=self.address)
         line = self._write_and_read(command, retries)
         spl = line.split()
+        print
+        print line
         address, values = spl[0], spl[1:]
         if address != self.address:
             print address, self.address
@@ -149,6 +151,8 @@ class FlowController(FlowMeter):
         """
         command = "{addr}S{flow:.2f}\r\n".format(addr=self.address, flow=flow)
         line = self._write_and_read(command, retries)
+        sleep(0.05)
+        self.flush()
         print self.get()
         #if abs(float(line) - flow) > 0.01:
         #    raise IOError("Could not set flow.")
